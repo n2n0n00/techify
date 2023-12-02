88e96872788485140d6bc22b55e5e532b68b665f
AT <athinat690@gmail.com>
2023-12-02 21:21:00 +0000

    categories done

--- src/app/(pages)/[slug]/page.tsx
+++ src/app/(pages)/[slug]/page.tsx
@@ -15,6 +15,7 @@ import { Gutter } from '../../_components/Gutter'
 export const dynamic = 'force-dynamic'
 
 import classes from './index.module.scss'
+import Categories from '../../_components/Categories'
 
 export default async function Page({ params: { slug = 'home' } }) {
   const { isEnabled: isDraftMode } = draftMode()
@@ -55,7 +56,9 @@ export default async function Page({ params: { slug = 'home' } }) {
       {slug === 'home' ? (
         <section>
           <Hero {...hero} />
-          <Gutter>all other content will go in here</Gutter>
+          <Gutter className={classes.home}>
+            <Categories categories={categories} />
+          </Gutter>
         </section>
       ) : (
         <React.Fragment>
